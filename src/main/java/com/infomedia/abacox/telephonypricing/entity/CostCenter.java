82ff01ea09c37cd8814b2556f91158d93b14a72f
jhonbetancourt <jhon.betancourt@infomediaservice.com>
2025-04-01 14:24:30 -0500

    Update CostCenter.java

--- src/main/java/com/infomedia/abacox/telephonypricing/entity/CostCenter.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/entity/CostCenter.java
@@ -1,6 +1,7 @@
 package com.infomedia.abacox.telephonypricing.entity;
 
 import com.infomedia.abacox.telephonypricing.dto.superclass.ActivableDto;
+import com.infomedia.abacox.telephonypricing.entity.superclass.ActivableEntity;
 import com.infomedia.abacox.telephonypricing.entity.superclass.AuditedEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -19,7 +20,7 @@ import org.hibernate.annotations.ColumnDefault;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder(toBuilder = true)
-public class CostCenter extends ActivableDto {
+public class CostCenter extends ActivableEntity {
 
     /**
      * Primary key for the cost center.
