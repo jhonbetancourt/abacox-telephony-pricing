65952f365d2a1b00b9ab8434beeb97bcdbae8a30
jhonbetancourt <jhon.betancourt@infomediaservice.com>
2025-05-16 12:37:58 -0500

    Update Series.java

--- src/main/java/com/infomedia/abacox/telephonypricing/entity/Series.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/entity/Series.java
@@ -61,7 +61,7 @@ public class Series extends ActivableEntity {
      */
     @Column(name = "ndc", nullable = false)
     @ColumnDefault("0")
-    private String ndc;
+    private Integer ndc;
 
     /**
      * Initial number in the series range.
@@ -69,7 +69,7 @@ public class Series extends ActivableEntity {
      */
     @Column(name = "initial_number", nullable = false)
     @ColumnDefault("0")
-    private String initialNumber;
+    private Integer initialNumber;
 
     /**
      * Final number in the series range.
@@ -77,7 +77,7 @@ public class Series extends ActivableEntity {
      */
     @Column(name = "final_number", nullable = false)
     @ColumnDefault("0")
-    private String finalNumber;
+    private Integer finalNumber;
 
     /**
      * Company associated with this number series.
