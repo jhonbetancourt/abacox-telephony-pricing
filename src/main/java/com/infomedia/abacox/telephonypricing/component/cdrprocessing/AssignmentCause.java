800e632b850c5c18042537bea4e87fdf03cf9365
jhonbetancourt <jhon.betancourt@infomediaservice.com>
2025-06-09 20:19:36 -0500

    Update AssignmentCause.java

--- src/main/java/com/infomedia/abacox/telephonypricing/component/cdrprocessing/AssignmentCause.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/component/cdrprocessing/AssignmentCause.java
@@ -2,21 +2,22 @@ package com.infomedia.abacox.telephonypricing.component.cdrprocessing;
 
 // Based on IMDEX_ASIGNA_* constants
 public enum AssignmentCause {
-    NOT_ASSIGNED(0), // Default or if no specific cause
-    EXTENSION(1),    // IMDEX_ASIGNA_EXT
+    NOT_ASSIGNED(null), // Default or if no specific cause
+    EXTENSION(0),    // IMDEX_ASIGNA_EXT
     AUTH_CODE(2),    // IMDEX_ASIGNA_CLAVE
-    TRANSFER(3),     // IMDEX_ASIGNA_TRANS
+    TRANSFER(5),     // IMDEX_ASIGNA_TRANS
     CONFERENCE(4),   // IMDEX_ASIGNA_CONFE
-    IGNORED_AUTH_CODE(5), // IMDEX_IGNORA_CLAVE
-    RANGES(6);       // IMDEX_ASIGNA_RANGOS
+    IGNORED_AUTH_CODE(6), // IMDEX_IGNORA_CLAVE
+    RANGES(3);       // IMDEX_ASIGNA_RANGOS
 
-    private final int value;
+    private final Integer value;
 
-    AssignmentCause(int value) {
+    AssignmentCause(Integer value) {
         this.value = value;
     }
 
-    public int getValue() {
+    public Integer getValue() {
         return value;
     }
+
 }
