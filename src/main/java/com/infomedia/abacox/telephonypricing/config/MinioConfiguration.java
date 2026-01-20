3606744d4e2dbc440d77b888f2c42f57e168ff9c
jhonbetancourt <jhon.betancourt@infomediaservice.com>
2026-01-20 12:58:24 -0500

    Update MinioConfiguration.java

--- src/main/java/com/infomedia/abacox/telephonypricing/config/MinioConfiguration.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/config/MinioConfiguration.java
@@ -1,9 +1,12 @@
 package com.infomedia.abacox.telephonypricing.config;
 
 import io.minio.MinioClient;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MinioConfiguration {
@@ -19,9 +22,16 @@ public class MinioConfiguration {
 
     @Bean
     public MinioClient minioClient() {
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS) // Handshake time
+                .writeTimeout(10, TimeUnit.MINUTES)   // Time allowed to write data (upload)
+                .readTimeout(10, TimeUnit.MINUTES)    // Time allowed to read response
+                .build();
+
         return MinioClient.builder()
                 .endpoint(url)
                 .credentials(accessKey, secretKey)
+                .httpClient(httpClient)
                 .build();
     }
 }
