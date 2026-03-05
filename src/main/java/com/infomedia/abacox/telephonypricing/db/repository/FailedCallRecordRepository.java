0bb96fc0fa3845a1d941dd4977b42cf270d812e9
jhonbetancourt <jhon.betancourt@infomediaservice.com>
2026-03-05 11:56:35 -0500

    update

--- src/main/java/com/infomedia/abacox/telephonypricing/db/repository/FailedCallRecordRepository.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/db/repository/FailedCallRecordRepository.java
@@ -4,5 +4,6 @@ import com.infomedia.abacox.telephonypricing.db.entity.FailedCallRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface FailedCallRecordRepository extends JpaRepository<FailedCallRecord, Long>, JpaSpecificationExecutor<FailedCallRecord> {
+public interface FailedCallRecordRepository extends JpaRepository<FailedCallRecord, Long>,
+        JpaSpecificationExecutor<FailedCallRecord>, SliceableSpecificationExecutor<FailedCallRecord> {
 }
--- src/main/java/com/infomedia/abacox/telephonypricing/db/repository/ReportRepository.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/db/repository/ReportRepository.java
@@ -3,8 +3,8 @@ package com.infomedia.abacox.telephonypricing.db.repository;
 import com.infomedia.abacox.telephonypricing.db.projection.*;
 import com.infomedia.abacox.telephonypricing.db.util.VirtualEntity;
 import com.infomedia.abacox.telephonypricing.db.repository.query.*;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,16 +21,16 @@ public interface ReportRepository extends JpaRepository<VirtualEntity, Long> {
                         @Param("extension") String extension,
                         @Param("employeeName") String employeeName);
 
-        @Query(value = EmployeeActivityReportQueries.QUERY, countQuery = EmployeeActivityReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<EmployeeActivityReport> getEmployeeActivityReport(
+        @Query(value = EmployeeActivityReportQueries.QUERY, nativeQuery = true)
+        Slice<EmployeeActivityReport> getEmployeeActivityReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("employeeName") String employeeName,
                         @Param("extension") String extension,
                         Pageable pageable);
 
-        @Query(value = EmployeeCallReportQueries.QUERY, countQuery = EmployeeCallReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<EmployeeCallReport> getEmployeeCallReport(
+        @Query(value = EmployeeCallReportQueries.QUERY, nativeQuery = true)
+        Slice<EmployeeCallReport> getEmployeeCallReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("employeeName") String employeeName,
@@ -43,8 +43,8 @@ public interface ReportRepository extends JpaRepository<VirtualEntity, Long> {
                         @Param("endDate") LocalDateTime endDate,
                         @Param("employeeIds") List<Long> employeeIds);
 
-        @Query(value = UnassignedCallReportQueries.QUERY, countQuery = UnassignedCallReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<UnassignedCallReport> getUnassignedCallReport(
+        @Query(value = UnassignedCallReportQueries.QUERY, nativeQuery = true)
+        Slice<UnassignedCallReport> getUnassignedCallReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("extension") String extension,
@@ -53,71 +53,71 @@ public interface ReportRepository extends JpaRepository<VirtualEntity, Long> {
                         @Param("maxLength") Integer maxLength,
                         Pageable pageable);
 
-        @Query(value = ProcessingFailureReportQueries.QUERY, countQuery = ProcessingFailureReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<ProcessingFailureReport> getProcessingFailureReport(
+        @Query(value = ProcessingFailureReportQueries.QUERY, nativeQuery = true)
+        Slice<ProcessingFailureReport> getProcessingFailureReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("directory") String directory,
                         @Param("errorType") String errorType,
                         Pageable pageable);
 
-        @Query(value = MissedCallEmployeeReportQueries.QUERY, countQuery = MissedCallEmployeeReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<MissedCallEmployeeReport> getMissedCallEmployeeReport(
+        @Query(value = MissedCallEmployeeReportQueries.QUERY, nativeQuery = true)
+        Slice<MissedCallEmployeeReport> getMissedCallEmployeeReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("employeeName") String employeeName,
                         @Param("minRingCount") Integer minRingCount,
                         Pageable pageable);
 
-        @Query(value = UnusedExtensionReportQueries.QUERY, countQuery = UnusedExtensionReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<UnusedExtensionReport> getUnusedExtensionReport(
+        @Query(value = UnusedExtensionReportQueries.QUERY, nativeQuery = true)
+        Slice<UnusedExtensionReport> getUnusedExtensionReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("employeeName") String employeeName,
                         @Param("extension") String extension,
                         Pageable pageable);
 
-        @Query(value = SubdivisionUsageReportQueries.QUERY, countQuery = SubdivisionUsageReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<SubdivisionUsageReport> getSubdivisionUsageReport(
+        @Query(value = SubdivisionUsageReportQueries.QUERY, nativeQuery = true)
+        Slice<SubdivisionUsageReport> getSubdivisionUsageReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("parentSubdivisionId") Long parentSubdivisionId,
                         Pageable pageable);
 
-        @Query(value = SubdivisionUsageByTypeReportQueries.QUERY, countQuery = SubdivisionUsageByTypeReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<SubdivisionUsageByTypeReport> getSubdivisionUsageByTypeReport(
+        @Query(value = SubdivisionUsageByTypeReportQueries.QUERY, nativeQuery = true)
+        Slice<SubdivisionUsageByTypeReport> getSubdivisionUsageByTypeReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("subdivisionIds") List<Long> subdivisionIds,
                         Pageable pageable);
 
-        @Query(value = TelephonyTypeUsageReportQueries.QUERY, countQuery = TelephonyTypeUsageReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<TelephonyTypeUsageReport> getTelephonyTypeUsageReport(
+        @Query(value = TelephonyTypeUsageReportQueries.QUERY, nativeQuery = true)
+        Slice<TelephonyTypeUsageReport> getTelephonyTypeUsageReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         Pageable pageable);
 
-        @Query(value = MonthlyTelephonyTypeUsageReportQueries.QUERY, countQuery = MonthlyTelephonyTypeUsageReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<MonthlyTelephonyTypeUsageReport> getMonthlyTelephonyTypeUsageReport(
+        @Query(value = MonthlyTelephonyTypeUsageReportQueries.QUERY, nativeQuery = true)
+        Slice<MonthlyTelephonyTypeUsageReport> getMonthlyTelephonyTypeUsageReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         Pageable pageable);
 
-        @Query(value = CostCenterUsageReportQueries.QUERY, countQuery = CostCenterUsageReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<CostCenterUsageReport> getCostCenterUsageReport(
+        @Query(value = CostCenterUsageReportQueries.QUERY, nativeQuery = true)
+        Slice<CostCenterUsageReport> getCostCenterUsageReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("parentCostCenterId") Long parentCostCenterId,
                         Pageable pageable);
 
-        @Query(value = EmployeeAuthCodeUsageReportQueries.QUERY, countQuery = EmployeeAuthCodeUsageReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<EmployeeAuthCodeUsageReport> getEmployeeAuthCodeUsageReport(
+        @Query(value = EmployeeAuthCodeUsageReportQueries.QUERY, nativeQuery = true)
+        Slice<EmployeeAuthCodeUsageReport> getEmployeeAuthCodeUsageReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         Pageable pageable);
 
-        @Query(value = MonthlySubdivisionUsageReportQueries.QUERY, countQuery = MonthlySubdivisionUsageReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<MonthlySubdivisionUsage> getMonthlySubdivisionUsageReport(
+        @Query(value = MonthlySubdivisionUsageReportQueries.QUERY, nativeQuery = true)
+        Slice<MonthlySubdivisionUsage> getMonthlySubdivisionUsageReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("subdivisionIds") List<Long> subdivisionIds,
@@ -129,26 +129,26 @@ public interface ReportRepository extends JpaRepository<VirtualEntity, Long> {
                         @Param("endDate") LocalDateTime endDate,
                         @Param("subdivisionIds") List<Long> subdivisionIds);
 
-        @Query(value = DialedNumberUsageReportQueries.QUERY, countQuery = DialedNumberUsageReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<DialedNumberUsageReport> getDialedNumberUsageReport(
+        @Query(value = DialedNumberUsageReportQueries.QUERY, nativeQuery = true)
+        Slice<DialedNumberUsageReport> getDialedNumberUsageReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         Pageable pageable);
 
-        @Query(value = DestinationUsageReportQueries.QUERY, countQuery = DestinationUsageReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<DestinationUsageReport> getDestinationUsageReport(
+        @Query(value = DestinationUsageReportQueries.QUERY, nativeQuery = true)
+        Slice<DestinationUsageReport> getDestinationUsageReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         Pageable pageable);
 
-        @Query(value = HighestConsumptionEmployeeReportQueries.QUERY, countQuery = HighestConsumptionEmployeeReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<HighestConsumptionEmployeeReport> getHighestConsumptionEmployeeReport(
+        @Query(value = HighestConsumptionEmployeeReportQueries.QUERY, nativeQuery = true)
+        Slice<HighestConsumptionEmployeeReport> getHighestConsumptionEmployeeReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         Pageable pageable);
 
-        @Query(value = ExtensionGroupReportQueries.QUERY, countQuery = ExtensionGroupReportQueries.COUNT_QUERY, nativeQuery = true)
-        Page<ExtensionGroupReport> getExtensionGroupReport(
+        @Query(value = ExtensionGroupReportQueries.QUERY, nativeQuery = true)
+        Slice<ExtensionGroupReport> getExtensionGroupReport(
                         @Param("startDate") LocalDateTime startDate,
                         @Param("endDate") LocalDateTime endDate,
                         @Param("extensions") List<String> extensions,
