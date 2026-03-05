8689d53e3a5c9a8ee275f3ac00ba20be46fe5698
jhonbetancourt <jhon.betancourt@infomediaservice.com>
2026-03-05 13:07:06 -0500

    Update TrunkRule.java

--- src/main/java/com/infomedia/abacox/telephonypricing/db/entity/TrunkRule.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/db/entity/TrunkRule.java
@@ -10,13 +10,10 @@ import org.hibernate.annotations.ColumnDefault;
 import java.math.BigDecimal;
 
 @Entity
-@Table(
-    name = "trunk_rule",
-    indexes = {
-        // Critical for getAppliedTrunkRule query
-        @Index(name = "idx_trunk_rule_lookup", columnList = "trunk_id, telephony_type_id, origin_indicator_id, active")
-    }
-)
+@Table(name = "trunk_rule", indexes = {
+                // Critical for getAppliedTrunkRule query
+                @Index(name = "idx_trunk_rule_lookup", columnList = "trunk_id, telephony_type_id, origin_indicator_id, active")
+})
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,92 +21,60 @@ import java.math.BigDecimal;
 @SuperBuilder(toBuilder = true)
 public class TrunkRule extends ActivableEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "trunk_rule_id_seq")
-    @SequenceGenerator(
-            name = "trunk_rule_id_seq",
-            sequenceName = "trunk_rule_id_seq",
-            allocationSize = 1,
-            initialValue = 10000000
-    )
-    @Column(name = "id", nullable = false)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "trunk_rule_id_seq")
+        @SequenceGenerator(name = "trunk_rule_id_seq", sequenceName = "trunk_rule_id_seq", allocationSize = 1, initialValue = 10000000)
+        @Column(name = "id", nullable = false)
+        private Long id;
 
-    @Column(name = "rate_value", nullable = false, precision = 10, scale = 4)
-    @ColumnDefault("0")
-    private BigDecimal rateValue;
+        @Column(name = "rate_value", nullable = false, precision = 10, scale = 4)
+        @ColumnDefault("0")
+        private BigDecimal rateValue;
 
-    @Column(name = "includes_vat", nullable = false)
-    @ColumnDefault("false")
-    private Boolean includesVat;
+        @Column(name = "includes_vat", nullable = false)
+        @ColumnDefault("false")
+        private Boolean includesVat;
 
-    @Column(name = "telephony_type_id")
-    private Long telephonyTypeId;
+        @Column(name = "telephony_type_id")
+        private Long telephonyTypeId;
 
-    @ManyToOne
-    @JoinColumn(
-            name = "telephony_type_id",
-            insertable = false,
-            updatable = false,
-            foreignKey = @ForeignKey(name = "fk_trunk_rule_telephony_type")
-    )
-    private TelephonyType telephonyType;
+        @ManyToOne
+        @JoinColumn(name = "telephony_type_id", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_trunk_rule_telephony_type"))
+        private TelephonyType telephonyType;
 
-    @Column(name = "indicator_ids", length = 255, nullable = false)
-    @ColumnDefault("''")
-    private String indicatorIds;
+        @Column(name = "indicator_ids", length = 255, nullable = false)
+        @ColumnDefault("''")
+        private String indicatorIds;
 
-    @Column(name = "trunk_id")
-    private Long trunkId;
+        @Column(name = "trunk_id")
+        private Long trunkId;
 
-    @ManyToOne
-    @JoinColumn(
-            name = "trunk_id",
-            insertable = false,
-            updatable = false,
-            foreignKey = @ForeignKey(name = "fk_trunk_rule_trunk")
-    )
-    private Trunk trunk;
+        @ManyToOne
+        @JoinColumn(name = "trunk_id", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_trunk_rule_trunk"))
+        private Trunk trunk;
 
-    @Column(name = "new_operator_id", nullable = false)
-    @ColumnDefault("0")
-    private Long newOperatorId;
+        @Column(name = "new_operator_id", nullable = false)
+        private Long newOperatorId;
 
-    @ManyToOne
-    @JoinColumn(
-            name = "new_operator_id",
-            insertable = false,
-            updatable = false,
-            foreignKey = @ForeignKey(name = "fk_trunk_rule_new_operator")
-    )
-    private Operator newOperator;
+        @ManyToOne
+        @JoinColumn(name = "new_operator_id", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_trunk_rule_new_operator"))
+        private Operator newOperator;
 
-    @Column(name = "new_telephony_type_id", nullable = false)
-    @ColumnDefault("0")
-    private Long newTelephonyTypeId;
+        @Column(name = "new_telephony_type_id", nullable = false)
+        private Long newTelephonyTypeId;
 
-    @ManyToOne
-    @JoinColumn(
-            name = "new_telephony_type_id",
-            insertable = false,
-            updatable = false,
-            foreignKey = @ForeignKey(name = "fk_trunk_rule_new_telephony_type")
-    )
-    private TelephonyType newTelephonyType;
+        @ManyToOne
+        @JoinColumn(name = "new_telephony_type_id", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_trunk_rule_new_telephony_type"))
+        private TelephonyType newTelephonyType;
 
-    @Column(name = "seconds", nullable = false)
-    @ColumnDefault("0")
-    private Integer seconds;
+        @Column(name = "seconds", nullable = false)
+        @ColumnDefault("0")
+        private Integer seconds;
 
-    @Column(name = "origin_indicator_id")
-    private Long originIndicatorId;
+        @Column(name = "origin_indicator_id")
+        private Long originIndicatorId;
 
-    @ManyToOne
-    @JoinColumn(
-            name = "origin_indicator_id",
-            insertable = false,
-            updatable = false,
-            foreignKey = @ForeignKey(name = "fk_trunk_rule_origin_indicator")
-    )
-    private Indicator originIndicator;
+        @ManyToOne
+        @JoinColumn(name = "origin_indicator_id", insertable = false, updatable = false, foreignKey = @ForeignKey(name = "fk_trunk_rule_origin_indicator"))
+        private Indicator originIndicator;
 }
