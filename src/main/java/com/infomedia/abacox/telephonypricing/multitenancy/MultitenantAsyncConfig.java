c84d599c053bab26f199f0d94df5e4d040adae35
jhonbetancourt <jhon.betancourt@infomediaservice.com>
2026-04-07 15:28:20 -0500

    update

--- src/main/java/com/infomedia/abacox/telephonypricing/multitenancy/MultitenantAsyncConfig.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/multitenancy/MultitenantAsyncConfig.java
@@ -2,15 +2,19 @@ package com.infomedia.abacox.telephonypricing.multitenancy;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAsync
-public class MultitenantAsyncConfig {
-   /* @Bean
-    public ThreadPoolTaskExecutor taskExecutor() {
+public class MultitenantAsyncConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setTaskDecorator(new TenantAwareTaskDecorator());
         executor.initialize();
-        return executor;
-    }*/
+        configurer.setTaskExecutor(executor);
+    }
 }
