7e996eaae1aafa87a8e5da5bf408f9690b110459
jhonbetancourt <jhon.betancourt@infomediaservice.com>
2026-04-13 11:26:17 -0500

    update

--- src/main/java/com/infomedia/abacox/telephonypricing/Application.java
+++ src/main/java/com/infomedia/abacox/telephonypricing/Application.java
@@ -6,22 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import jakarta.annotation.PostConstruct;
-
 @SpringBootApplication
 @EnableScheduling
 public class Application {
+
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota")); 
+    }
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	 @PostConstruct
-    public void init() {
-        // Force the JVM to match the legacy PHP server's timezone
-        // This ensures all new inserts/updates match the legacy database data
-        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota")); 
-        
-        // Note: You can also inject a property here if you want it configurable:
-        // TimeZone.setDefault(TimeZone.getTimeZone(myConfiguredTimezone));
-    }
 }
